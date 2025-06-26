5f010b15a5dd16f004d5a5121a79f6b342a2bba2
Itay Katav <ikatav@redhat.com>
2025-06-26 14:11:53 +0300

    feat: implement new user, and align feast service
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- backend/services/feast_service.py
+++ backend/services/feast_service.py
@@ -1,6 +1,14 @@
 from typing import List
 from feast import FeatureStore
 from models import Product, User
+import os
+from minio import Minio
+import torch
+import json
+import shutil
+from public.models.entity_tower import EntityTower
+from public.models.data_util import data_preproccess
+import pandas as pd
 
 class FeastService:
     _instance = None
@@ -10,13 +18,40 @@ class FeastService:
             cls._instance = super(FeastService, cls).__new__(cls)
             cls._instance._initialized = False
         return cls._instance
-    
+
     def __init__(self):
         if not self._initialized:
-            self.store = FeatureStore('')
+            self.store = FeatureStore('public')
             self._initialized = True
+            self.user_encoder = self._load_user_encoder()
+
+    def _load_user_encoder(self):
+        minio_client = Minio(
+            endpoint=os.getenv('MINIO_HOST', "endpoint") + \
+                ':' + os.getenv('MINIO_PORT', '9000'),
+            access_key=os.getenv('MINIO_ACCESS_KEY', "access-key"),
+            secret_key=os.getenv('MINIO_SECRET_KEY', "secret-key"),
+            secure=False  # Set to True if using HTTPS
+        )
+        
+        bucket_name = "user-encoder"
+        object_name = "user-encoder.pth"
+        configuration = 'user-encoder-config.json'
+        
+        # Download the model file from MinIO
+        minio_client.fget_object(bucket_name, object_name, "/tmp/user-encoder.pth")
+        minio_client.fget_object(bucket_name, configuration, "/tmp/user-encoder-config.json")
+        # Load the model state dict using torch.load
+        with open('/tmp/user-encoder-config.json', 'r') as f:
+            json_config = json.load(f)
+        user_encoder = EntityTower(json_config['users_num_numerical'], json_config['users_num_categorical'])
+        user_encoder.load_state_dict(torch.load('/tmp/user-encoder.pth'))
+        # Clean up the temporary folder
+        shutil.rmtree("/tmp")
+        
+        return user_encoder
     
-    def load_items_exiting_user(self, user_id: int) -> List[Product]:
+    def load_items_existing_user(self, user_id: int) -> List[Product]:
         suggested_item_ids = self.store.get_online_features(
             features=self.store.get_feature_service('user_top_k_items'),
             entity_rows=[{'user_id': user_id}]
@@ -24,32 +59,46 @@ class FeastService:
         
         top_item_ids = suggested_item_ids.to_df().iloc[0]['top_k_item_ids']
         
+        return self._item_ids_to_product_list(top_item_ids)
+
+
+    def load_items_new_user(self, user: User, k: int = 10):
+        user_as_df = pd.DataFrame([user.model_dump()])
+        
+        user_embed = self.user_encoder(**data_preproccess(user_as_df))[0]
+        top_k = self.store.retrieve_online_documents(
+            query=user_embed.tolist(),
+            top_k=k,
+            features=['item_embedding:item_id']
+        )
+        top_item_ids = top_k.to_df().iloc[0]['top_k_item_ids']
+
+        return self._item_ids_to_product_list(top_item_ids)
+    
+    def _item_ids_to_product_list(self, top_item_ids: pd.Series | List) -> List[Product]:
+    
         suggested_item = self.store.get_online_features(
             features=self.store.get_feature_service('item_service'),
             entity_rows=[{'item_id': item_id}  for item_id in top_item_ids]
         ).to_df()
-        return suggested_item
-    
-    def load_items_new_user(self, user: User): # TODO fix me
-        # TODO finish logic
-        pass
         
-        # num_new_user = 1
-        # k = 10
-        # new_users = generate_users(num_new_user, num_users + 1)
-        # new_users_embeddings = user_encoder(**data_preproccess(new_users))
-        # top_k_items = []
-        # for user_embed in new_users_embeddings:
-        #     top_k = self.store.retrieve_online_documents(
-        #         query=user_embed.tolist(),
-        #         top_k=k,
-        #         # feature=f'{item_embedding_view}:item_id'
-        #         features=[f'{item_embedding_view}:item_id']
-        #     )
-        #     top_k_items.append(top_k.to_df())
-
-        # new_users['top_k_items'] = top_k_items
-        # new_users[['item_id', 'top_k_items']]
-
+        print(suggested_item.columns)
+        print(suggested_item)
+        # Parse df to list of products
+        suggested_item = [Product(
+            item_id=row.item_id,
+            name=row.name,
+            category=row.category,
+            subcategory=row.subcategory, 
+            price=row.price,
+            avg_rating=row.avg_rating,
+            num_ratings=row.num_ratings,
+            popular=row.popular,
+            new_arrival=row.new_arrival,
+            on_sale=row.on_sale,
+            arrival_date=row.arrival_date,
+            description=row.description
+        ) for row in suggested_item.itertuples()]
+        return suggested_item
 # Initilaize the service
 feast_service = FeastService()
